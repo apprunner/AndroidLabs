7f1a2ee81f28a90d4c0c4b54a94f982919a1d9ba
apple <apple@172.20.10.4>
2014-04-28 16:01:07 +0800

    1.0.0.2 - add unregister receiver method

--- Boardcast01/src/com/example/boardcast01/MainActivity.java
+++ Boardcast01/src/com/example/boardcast01/MainActivity.java
@@ -28,19 +28,19 @@ public class MainActivity extends ActionBarActivity {
 
         //register a Broadcast Receiver
         IntentFilter filter = new IntentFilter(myAction);
-        BroadcastReceiver receiver = new BroadcastReceiver(){
+        receiver = new BroadcastReceiver(){
 			@Override
 			public void onReceive(Context context, Intent intent) {
 				//receive the broadcast information
 				
-				String action = intent.getAction();	//get action
-				//String data = intent.getExtras().getString("data");
+				String action = intent.getAction();	//get intent action
+				String data = intent.getExtras().getString("data");	//get intent data
 				
 				if (action.equals(myAction))
 				{
 			        //setting the Basic Toast information
 			        Context ctx = getApplicationContext();
-			        CharSequence info = "接收到廣播:" + myAction ;
+			        CharSequence info = "接收到廣播:" + action + ",資料的內容是:" + data;
 			        int duration = Toast.LENGTH_SHORT;
 			        
 			        Toast toast = Toast.makeText(ctx, info, duration);
@@ -54,12 +54,14 @@ public class MainActivity extends ActionBarActivity {
         
         //sending broadcast
         Button btnSend = (Button)findViewById(R.id.btnSend);
-        
         btnSend.setOnClickListener(new View.OnClickListener() {
 			@Override
 			public void onClick(View v) {
+				//setting the Intent
+				Intent intent = new Intent(myAction);
+				intent.putExtra("data", "這是我自訂的Action!");
 				// sending the broadcast
-				sendBroadcast(new Intent(myAction));
+				sendBroadcast(intent);		
 			}
 		});
     }
@@ -84,4 +86,15 @@ public class MainActivity extends ActionBarActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+
+	@Override
+	protected void onStop() {
+		super.onStop();
+		try{
+			this.unregisterReceiver(receiver); 	//unregister the receiver
+		}catch(IllegalArgumentException ex){
+			Toast.makeText(getApplicationContext(), ex.toString(), Toast.LENGTH_SHORT).show();
+		}
+	}
 }
